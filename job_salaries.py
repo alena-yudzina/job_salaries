149e33e412bd36b928b5b6ee1d4e8aa32f392df6
uzapolsky <uzapolsky@gmail.com>
2021-09-09 11:54:32 +0300

    turn out creating request bodies from cycles and dried the code

--- job_salaries.py
+++ job_salaries.py
@@ -28,8 +28,7 @@ def predict_rub_salary_sj(vacancy):
     salary_from = vacancy['payment_from']
     salary_to = vacancy['payment_to']
     currency = vacancy['currency']
-    is_payment_filled = salary_from or salary_to
-    if not is_payment_filled or currency != 'rub':
+    if currency != 'rub':
         return None
     return predict_salary(salary_from, salary_to)
 
@@ -41,7 +40,7 @@ def process_stats_params(vacancies, predict_salary_func):
         vacancy_salaries.append(salary)
 
     vacancy_salaries = list(filter(bool, vacancy_salaries))
-    if len(vacancy_salaries) != 0:
+    if vacancy_salaries:
         average_salary = sum(vacancy_salaries) / len(vacancy_salaries)
     else:
         average_salary = 0
@@ -49,22 +48,23 @@ def process_stats_params(vacancies, predict_salary_func):
     return average_salary, vacancies_processed
 
 
-def get_site_stats_hh(*args, language='Python'):
+def get_site_stats_hh(token, language='Python'):
     url = 'https://api.hh.ru/vacancies'
 
     vacancies = []
-    MOSCOW_ID = 1
-    SEARCHING_PERIOD = 30
-    VACANCIES_PER_PAGE = 100
+    moscow_id = 1
+    searching_period = 30
+    vacancies_per_page = 100
+    payload = {
+            'text': 'программист {0}'.format(language),
+            'area': moscow_id,
+            'period': searching_period,
+            'per_page': vacancies_per_page,
+        }
+
     for page in count(0):
 
-        payload = {
-            'text': 'программист {0}'.format(language),
-            'area': MOSCOW_ID,
-            'period': SEARCHING_PERIOD,
-            'page': page,
-            'per_page': VACANCIES_PER_PAGE,
-        }
+        payload['page'] = page
 
         response = requests.get(url, params=payload)
         response.raise_for_status()
@@ -74,33 +74,38 @@ def get_site_stats_hh(*args, language='Python'):
         if page >= response_body['pages'] - 1:
             break
     found_vacancies = response_body['found']
-    return *process_stats_params(vacancies, predict_rub_salary_hh), found_vacancies
+    average_salary, vacancies_processed = process_stats_params(vacancies, predict_rub_salary_hh)
+    return average_salary, vacancies_processed, found_vacancies
 
 
 def get_site_stats_sj(token, language='Python'):
     api_url = 'https://api.superjob.ru/2.0/vacancies/'
+
+    searching_period = 30
+    it_department_id = 48
+    moscow_id = 4
+    vacancies_per_page = 100
+    serach_position_id = 1
+
+    headers = {
+            'X-Api-App-Id': token,
+    }
+
+    payload = {
+        'period': searching_period,
+        'catalogues': it_department_id,
+        'town': moscow_id,
+        'count': vacancies_per_page,
+        'keywords[0][keys]': language,
+        'keywords[0][skwc]': 'particular',
+        'keywords[0][srws]': serach_position_id,
+    }
+
     vacancies = []
-    SEARCHING_PERIOD = 30
-    IT_DEPARTMENT_ID = 48
-    MOSCOW_ID = 4
-    VACANCIES_PER_PAGE = 100
-    SERCH_POSITION_ID = 1
+
     for page in count(0):
 
-        headers = {
-            'X-Api-App-Id': token,
-        }
-
-        payload = {
-            'period': SEARCHING_PERIOD,
-            'catalogues': IT_DEPARTMENT_ID,
-            'town': MOSCOW_ID,
-            'page': page,
-            'count': VACANCIES_PER_PAGE,
-            'keywords[0][keys]': language,
-            'keywords[0][skwc]': 'particular',
-            'keywords[0][srws]': SERCH_POSITION_ID,
-        }
+        payload['page'] = page
 
         response = requests.get(api_url, headers=headers, params=payload)
         response.raise_for_status()
@@ -110,13 +115,15 @@ def get_site_stats_sj(token, language='Python'):
         if not response_body['more']:
             break
     found_vacancies = response_body['total']
-    return *process_stats_params(vacancies, predict_rub_salary_sj), found_vacancies
+    average_salary, vacancies_processed = process_stats_params(vacancies, predict_rub_salary_sj)
+    return average_salary, vacancies_processed, found_vacancies
 
 
 def create_stats(languages, site_stats_func, token=''):
     languages_stats = {}
     for language in languages:
-        average_salary, vacancies_processed, found_vacancies = site_stats_func(token, language=language)
+        average_salary, vacancies_processed, found_vacancies = \
+            site_stats_func(token=token, language=language)
         languages_stats[language] = {
             'vacancies_found': found_vacancies,
             'vacancies_processed': vacancies_processed,
